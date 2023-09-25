ffca19dec604fe552eb737003ca6010bf619ef48
bitebait <william@schwaab.me>
2023-09-24 23:01:11 -0300

    refact: error handling and improved functionality

--- dolarpy.go
+++ dolarpy.go
@@ -1,81 +1,103 @@
 package dolarpy
 
-/*
-This is a Golang implementation of the
-Dolarpy API (https://github.com/melizeche/dolarPy -  melizeche )
-*/
-
 import (
 	"encoding/json"
-	"io"
-	"log"
+	"fmt"
 	"net/http"
 )
 
-type apiData map[string]map[string]map[string]float64
+// APIData represents the structure of the API response
+type APIData struct {
+	Dolarpy map[string]map[string]float64 `json:"dolarpy"`
+}
 
-func getAPIData() apiData {
+// getAPIData retrieves the API data from the remote server
+func getAPIData() (*APIData, error) {
 	client := &http.Client{}
 
 	req, err := http.NewRequest("GET", "https://dolar.melizeche.com/api/1.0/", nil)
 	if err != nil {
-		log.Fatalln(err)
+		return nil, fmt.Errorf("failed to create request: %w", err)
 	}
 
 	req.Header.Set("User-Agent", "DolarpyWrapper/Golang")
 
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Fatalln(err)
+		return nil, fmt.Errorf("failed to send request: %w", err)
 	}
-
 	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
+
+	var data APIData
+	err = json.NewDecoder(resp.Body).Decode(&data)
 	if err != nil {
-		log.Fatalln(err)
+		return nil, fmt.Errorf("failed to decode response: %w", err)
 	}
 
-	data := apiData{}
-	json.Unmarshal(body, &data)
-
-	return data
+	return &data, nil
 }
 
-func Reference() float64 {
-	data := getAPIData()
-	return data["dolarpy"]["bcp"]["referencial_diario"]
+// Reference returns the referential exchange rate from BCP
+func Reference() (float64, error) {
+	data, err := getAPIData()
+	if err != nil {
+		return 0, fmt.Errorf("failed to get API data: %w", err)
+	}
+	return data.Dolarpy["bcp"]["referencial_diario"], nil
 }
 
-func Providers() []string {
-	data := getAPIData()
+// Providers returns a list of available providers
+func Providers() ([]string, error) {
+	data, err := getAPIData()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get API data: %w", err)
+	}
 
-	providers := []string{}
-	for key := range data["dolarpy"] {
+	providers := make([]string, 0, len(data.Dolarpy))
+	for key := range data.Dolarpy {
 		providers = append(providers, key)
 	}
 
-	return providers
+	return providers, nil
 }
 
-func Purchase(provider string) float64 {
-	data := getAPIData()
-	if provider == "" {
-		return data["dolarpy"]["bcp"]["compra"]
-	} else {
-		return data["dolarpy"][provider]["compra"]
+// Purchase returns the purchase exchange rate from the specified provider
+// If provider is empty, returns the rate from BCP
+func Purchase(provider string) (float64, error) {
+	data, err := getAPIData()
+	if err != nil {
+		return 0, fmt.Errorf("failed to get API data: %w", err)
 	}
-}
 
-func Sale(provider string) float64 {
-	data := getAPIData()
-	if provider == "" {
-		return data["dolarpy"]["bcp"]["venta"]
-	} else {
-		return data["dolarpy"][provider]["venta"]
+	rate := data.Dolarpy["bcp"]["compra"]
+	if provider != "" {
+		rate = data.Dolarpy[provider]["compra"]
 	}
+
+	return rate, nil
 }
 
-func All() map[string]map[string]float64 {
-	data := getAPIData()
-	return data["dolarpy"]
+// Sale returns the sale exchange rate from the specified provider
+// If provider is empty, returns the rate from BCP
+func Sale(provider string) (float64, error) {
+	data, err := getAPIData()
+	if err != nil {
+		return 0, fmt.Errorf("failed to get API data: %w", err)
+	}
+
+	rate := data.Dolarpy["bcp"]["venta"]
+	if provider != "" {
+		rate = data.Dolarpy[provider]["venta"]
+	}
+
+	return rate, nil
+}
+
+// All returns all available exchange rates from all providers
+func All() (map[string]map[string]float64, error) {
+	data, err := getAPIData()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get API data: %w", err)
+	}
+	return data.Dolarpy, nil
 }
